927e2c9c5ccfb64618d593b562e8e06ecd78cadd
li437 <qqli007009@gmail.com>
2013-06-24 22:33:42 +0800

    HelloWorld  hoho

---
+++ FirstGitJava/src/com/lz/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.lz;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: li437
+ * Date: 13-6-24
+ * Time: 下午10:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
